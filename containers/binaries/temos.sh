a6f5b8b55eecb71f4217ca9338cea0c37457b090
Luca Di Stefano <luca.di.stefano@tuwien.ac.at>
2025-03-14 15:15:18 +0000

    Fix temos script

--- containers/binaries/temos.sh
+++ containers/binaries/temos.sh
@@ -4,7 +4,7 @@ BASEPATH=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
 tmptsl=`mktemp --suffix .tsl`
 tmptlsf=`mktemp --suffix .tlsf`
 $BASEPATH/temos-tslmt2tsl $1 $BASEPATH/cvc5 > $tmptsl
-killall cvc5
+pkill -x cvc5
 $BASEPATH/temos-tsl2tlsf $tmptsl > $tmptlsf
 rm $tmptsl
 INS=$($BASEPATH/syfco -f ltl --print-input-signals $tmptlsf)
